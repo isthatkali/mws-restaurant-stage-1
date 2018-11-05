5fb603e6bae2d104483d9a38dad10664069ed16d
Kali <isthatkali@gmail.com>
2018-11-05 12:33:38 -0800

    reg service worker and created cache

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+// Add service worker (referenced: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/js/sw.js')
+  .then(ok => {
+    console.log('reg okay');
+  }).catch(err => {
+    console.log('error');
+  });
+}
+
+// Add cache of resources 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/10.jpg',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/index.html',
+        '/restaurant.html'
+      ]);
+    })
+  );
+});
+
+// Add responses to fetch events (referenced: https://matthewcranford.com/restaurant-reviews-app-walkthrough-part-4-service-workers/)
+
+
 let restaurants,
   neighborhoods,
   cuisines
